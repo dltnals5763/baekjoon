6cc5c45bc30d07e2b5aa915da41837e23eb34dca
sumin <dltnals5763@naver.com>
2022-12-30 22:01:00 +0900

    [백준]브론즈1 문제풀이

---
+++ src/source_221229/b1/B1_1037.java
@@ -0,0 +1,25 @@
+package source_221229.b1;
+
+
+import java.util.Scanner;
+
+public class B1_1037 {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int num = in.nextInt();
+
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+
+		while(num-- > 0) {
+			int N = in.nextInt();
+			max = Math.max(N, max);
+			min = Math.min(N, min);
+		}
+		System.out.println(max * min);
+	}
+
+}
---
+++ src/source_221229/b1/B1_1145.java
@@ -0,0 +1,41 @@
+package source_221229.b1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class B1_1145
+{
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		List<Integer> nums = new ArrayList<>();
+		for(int i=0; i<5; i++)
+		{
+			int num = Integer.parseInt(st.nextToken());
+			nums.add(num);
+		}
+
+		int a = 1;
+		while(true)
+		{
+			int cnt = 0;
+			for(int i=0; i<5; i++)
+			{
+				if(a % nums.get(i) == 0)
+					cnt ++;
+			}
+			if(cnt >= 3)
+			{
+				System.out.println(a);
+				break;
+			}
+			a ++;
+		}
+		br.close();
+	}
+}
