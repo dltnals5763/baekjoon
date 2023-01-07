18a6e025ae63b3a7c12ed3bad31df542f8ac2d75
sumin <dltnals5763@naver.com>
2023-01-07 23:46:21 +0900

    [백준]실버5 문제풀이

--- src/source_230107/s5/S5_1343.java
+++ src/source_230107/s5/S5_1343.java
@@ -4,46 +4,22 @@ package source_230107.s5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class S5_1343 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        String[] boards = s.split("\\.");
-        List<String> results = new ArrayList<>();
-        boolean isPossible = true;
-        for (String board : boards) {
-            int length = board.length();
-            if(length%2 != 0)
-            {
-                isPossible = false;
-                break;
-            }
-            else
-            {
-                StringBuilder sb = new StringBuilder();
-                int a = length / 4;
-                int b = length % 4;
-                for(int i=0; i<a; i++)
-                    sb.append("AAAA");
-                for(int i=0; i<b; i++)
-                    sb.append("B");
-                results.add(sb.toString()+".");
-            }
-        }
+        System.out.println(calculate(br.readLine()));
 
-        if(!isPossible)
-            System.out.println(-1);
-        else
-        {
-            StringBuilder out = new StringBuilder();
-            for(String result : results)
-            {
-                out.append(result).append(".");
-            }
-            System.out.println(out.substring(0, out.length()-1));
-        }
+    }
+    private static String calculate(String s) {
+        String ans;
+        String a = "AAAA";
+        String b = "BB";
+
+        s = s.replaceAll("XXXX", a);
+        ans = s.replaceAll("XX", b);
+        if(ans.contains("X"))
+            ans = "-1";
+        return ans;
     }
 }
