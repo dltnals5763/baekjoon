b53154515623bfd196314ed6baea48f4631b392a
sumin <dltnals5763@naver.com>
2023-01-29 17:37:10 +0900

    [백준]실버5 문제풀이

---
+++ src/source_230129/s5/S5_1436.java
@@ -0,0 +1,20 @@
+package source_230129.s5;
+
+import java.util.Scanner;
+
+public class S5_1436 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int ans = scan.nextInt();
+
+        int i = 665;
+        int n = 0;
+        do {
+            i++;
+            if (String.valueOf(i).contains("666")) {
+                n++;
+            }
+        } while (n != ans);
+        System.out.println(i);
+    }
+}
